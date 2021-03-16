d27e9ee442b1e25d0e4aeeb5342006c1c151cd37
fyodor <fyodor.zakharov@gmail.com>
2021-03-17 00:06:46 +0300

    start

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -2,11 +2,34 @@
 
 namespace HomeWork8
 {
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Group { get; set; }
+
+    }
+    class Order
+    {
+        public enum CoffeeDrinks
+        {
+            Espresso,
+            Americano,
+            Cappuccino
+        }
+
+        public Student Orderer { get; set; }
+        public CoffeeDrinks Drink { get; set; }
+
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
